6a8b16a5930332173e98fc26e70dcbe4908d09ed
malte.josten@uni-due.de <malte.josten@uni-due.de>
2023-06-13 16:19:58 +0200

    [CODE] Started working on automation for data collection (light controls)

---
+++ code/light_control.py
@@ -0,0 +1,40 @@
+import requests
+import time
+import random
+
+class LightControl:
+    api_point = "192.168.108.103:8080"
+    api_path  = "Shelly_PlugS_Power"
+
+    def turnOnOff(self, to):
+        state = "OFF"
+        if to:
+            state = "ON"
+        
+        res = requests.post(
+                "http://" + self.api_point + "/rest/items/" + self.api_path,
+                headers = {
+                    'Content-Type': 'text/plain',
+                    'Accept': 'application/json'
+                    },
+                data = state)
+
+        print(res)
+
+        if to:
+            print("Turned light on")
+        else:
+            print("Turned light off")
+
+    def chooseBehavior(self):
+        choice = random.randint(0,1)
+
+        self.turnOnOff((choice == 1))
+
+
+lc = LightControl()
+lc.turnOnOff(False)
+
+while(True):
+    time.sleep(5)
+    lc.chooseBehavior()
