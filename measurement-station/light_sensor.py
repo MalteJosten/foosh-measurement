e023276f91687007ce684778b0b30b0f092ea881
Malte Josten <malte.josten@uni-due.de>
2023-06-26 09:54:00 +0200

    Added multiple measurements and committing their average.

--- measurement-station/light_sensor.py
+++ measurement-station/light_sensor.py
@@ -1,11 +1,17 @@
-# Taken from peppe80 "How to use a photoresistor with Raspberry Pi Pico", https://peppe8o.com/how-to-use-a-photoresistor-with-raspberry-pi-pico/
-from machine import ADC, Pin
+# Inspired by peppe80 "How to use a photoresistor with Raspberry Pi Pico", https://peppe8o.com/how-to-use-a-photoresistor-with-raspberry-pi-pico/
+# and "Connecting to the Internet with Raspberry Pi Pico W', https://datasheets.raspberrypi.com/picow/connecting-to-the-internet-with-pico-w.pdf
+from machine import ADC, Pin, reset
 from time import sleep
 from math import pow
-import urequests
-import ujson
-import network
+from urequests import post
+from ujson import dumps
+from network import WLAN, STA_IF
 import secrets
+
+
+PROBES   = 10
+INTERVAL = 15
+REQUEST_URL = "http://192.168.108.65:3000/add"
 
 
 class LightSensor():
@@ -15,31 +21,43 @@ class LightSensor():
 
     def connect(self):
         try:
-            wlan = network.WLAN(network.STA_IF)
+            wlan = WLAN(STA_IF)
             wlan.active(True)
 
             if not(wlan.isconnected()):
                 wlan.connect(secrets.SSID, secrets.PWD)
 
-                while not(wlan.isconnected()):
+                timeout = 10
+                while timeout > 0:
+                    if (wlan.status() < 0 or wlan.status() > 3):
+                        break
+
+                    timeout -= 1
                     sleep(1)
+
+                if (wlan.status != 3):
+                    print("Could not connect to network.\n Resetting...")
+                    sleep(1)
+                    reset()
 
             self.has_connection = True
         except Exception as e:
             print("Network Error:", e)
             sleep(2)
-            machine.reset()
+            reset()
 
     def send(self, value):
         try:
-            request_url = "http://192.168.108.65:3000/add"
-            post_data = ujson.dumps({'value': value})
-            res = urequests.post(request_url, headers={'content-type': 'application/json'}, data=post_data)
+            post_data = dumps({'value': value})
+            res = post(
+                    REQUEST_URL,
+                    headers={'content-type': 'application/json'},
+                    data=post_data)
             res.close()
         except Exception as e:
             print("HTTP Error:", e)
             sleep(2)
-            machine.reset()
+            reset()
 
     def read_light(self):
         light = self.adc.read_u16()
@@ -52,6 +70,16 @@ class LightSensor():
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
+def average(list):
+    sum = 0
+    for x in list:
+        sum += x
+
+    avg = sum / len(list)
+
+    return avg
+
+
 ls = LightSensor(28)
 ls.connect()
 
@@ -59,5 +87,10 @@ while not(ls.has_connection):
     sleep(0.1)
 
 while True:
-    ls.send(ls.read_light())
-    sleep(2)
+    readings = []
+    for i in range(0, PROBES):
+        readings.append(ls.read_light())
+        sleep(1)
+
+    ls.send(average(readings))
+    sleep(INTERVAL)
