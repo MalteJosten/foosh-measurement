98d4df2fcad49f7833355992d3ffe72d223cff03
Malte Josten <malte.josten@uni-due.de>
2023-06-28 16:43:51 +0200

    Created Springboot Project

---
+++ foosh/src/main/java/com/vs/foosh/FooshApplication.java
@@ -0,0 +1,13 @@
+package com.vs.foosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooshApplication.class, args);
+	}
+
+}
---
+++ foosh/src/main/java/com/vs/foosh/api/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.vs.foosh.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "FooSH v0.0.1";
+    }
+    
+}
